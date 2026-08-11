46906d8ee1376cfb653da633ff78015e91551721
ccvv147mm-blip <ccvv147mm@gmail.com>
2026-08-11 10:11:37 +0300

    Update sign_app.py

--- sign_app.py
+++ sign_app.py
@@ -9,7 +9,6 @@ gradle_path = "android/app/build.gradle"
 with open(gradle_path, "r") as f:
     content = f.read()
 
-# التأكد من عدم تكرار الإضافة
 if "signingConfigs {" not in content:
     signing_block = f"""
     signingConfigs {{
@@ -21,11 +20,9 @@ if "signingConfigs {" not in content:
         }}
     }}
     """
-    # إدراج الـ signingConfigs قبل buildTypes مباشرة
     content = content.replace("buildTypes {", signing_block + "\n    buildTypes {", 1)
 
 if "signingConfig signingConfig.release" not in content:
-    # ربط release بـ signingConfig
     content = content.replace("release {", "release {\n            signingConfig signingConfig.release", 1)
 
 with open(gradle_path, "w") as f:
